3438772ae5e56fce6fa7bf70ee1051df53060237
Laurine <bouchartlaurine7@gmail.com>
2025-07-17 17:52:05 +0200

    Update: Add section

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      <h1>home</h1>
-    </div>
+    <main>
+      <section name="mission" id='select'>
+        <img src="./travail.png" alt="Personne de Wild Weave" />
+        <h1>Notre mission :</h1>
+        <p>Chez Wild Weave, nous croyons qu’un vêtement doit respecter celles et ceux qui le portent, mais aussi ceux qui le fabriquent  et la planète qui nous inspire. Nos tissus biologiques, nos ateliers locaux et nos choix durables façonnent chaque pièce, pour une mode plus juste et plus douce.</p>
+      </section>
+      <div className='slogan'>
+        <h1>Tissé par la nature, porté pour demain.</h1>
+      </div>
+      <section name="histoire" id="select">
+        <img src="./equipe.png" alt="Photo de l'équipe" />
+        <h1>Notre histoire</h1>
+        <p>L’aventure Wild Weave est née d’un constat simple : l’industrie textile doit changer. De la première fibre à l’ultime emballage, nous tissons des liens entre nature, savoir-faire artisanal et style intemporel. Notre équipe réunit créateurs, artisans et rêveurs, tous animés par l’envie de redonner du sens à ce que nous portons.</p>
+      </section>
+    </main>
   );
 };
 
