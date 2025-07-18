039ef79562bd788babad4e2cbf9534fb4cee7137
Laurine <bouchartlaurine7@gmail.com>
2025-07-18 18:42:10 +0200

    Update: responsive menu burger

--- src/page/component/Navbar.jsx
+++ src/page/component/Navbar.jsx
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav>
-      <ul>
+    <nav className="navbar">
+      <div className="burger" onClick={toggleMenu}>
+        <div className={`bar ${isOpen ? 'open' : ''}`}></div>
+        <div className={`bar ${isOpen ? 'open' : ''}`}></div>
+        <div className={`bar ${isOpen ? 'open' : ''}`}></div>
+      </div>
+      <ul className={`nav-links ${isOpen ? 'show' : ''}`}>
         <li><a href="/">Accueil</a></li>
-        <li><a href="/collection">Collections</a> </li>
-        <li><a href="/engagement">Engagements</a> </li>
-        <li><a href="/contact">Contact</a> </li>
+        <li><a href="/collection">Collections</a></li>
+        <li><a href="/engagement">Engagements</a></li>
+        <li><a href="/contact">Contact</a></li>
       </ul>
     </nav>
   );
