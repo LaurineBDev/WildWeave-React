3a39a3db8ed6dec16dee39af89e498cfa81a990e
Laurine <bouchartlaurine7@gmail.com>
2025-07-18 18:07:41 +0200

    WIP: sections + links

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
 
 const Contact = () => {
   return (
-    <div>
-      <h1>contact</h1>
-    </div>
+    <main className='contact'>
+      <div className="s-content">
+      <section className='container'>
+        <a className="toform" href="#">Contactez-nous</a>
+        <p>Une question, une suggestion, ou juste envie de nous dire bonjour ? L’équipe Wild Weave vous répondra dans les plus brefs délais.</p>
+      </section>
+      <section className="container">
+        <ul className="tel">
+          <li>📧 hello@wildweave.fr</li>
+          <li>🧵 Atelier Wild Weave 123 Rue du Lin, 75000 Paris</li>
+          <li>📞 +12 3 45 67 89 10</li>
+        </ul>
+      </section> 
+      </div>
+       <section className="follow">
+        <ul>
+         <li>👉 Suivez-nous :</li>
+         <li><a href="#"><img className="logo" src="./logo/instagram.png" alt="Lien vers notre compte Instagram" /></a></li>
+        </ul> 
+     </section>
+    </main>
   );
 };
 
